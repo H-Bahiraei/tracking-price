7773cc1b82b1790df11800cfcb9e458c06ff94b8
Mehrad <bahiraei.h@gmail.com>
2023-05-29 13:33:54 +0500

    adding git-flow, spring boot initializer. closes #2

---
+++ src/main/java/com/microservices/trackingprice/TrackingPriceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.trackingprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingPriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackingPriceApplication.class, args);
+    }
+
+}
